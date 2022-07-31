08e661d0e46773331426a8d07eac2c3cd416e239
Giovanni Mazzuoccolo <gio.mazzuoccolo@gmail.com>
2022-07-31 23:04:51 +0200

    hamburgher menu

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -1,15 +1,39 @@
 <template>
-  <nav>
-    <ul class="flex uppercase gap-4">
-      <li>Learn</li>
-      <li>About</li>
-      <li>Contacts</li> 
-      <li @click="$store.dispatch('authenticate')" class="text-main-orange">Join now</li>
-    </ul>
-  </nav>
+    <nav>
+        <svg viewBox="0 0 100 80" width="32" height="32">
+            <rect
+                width="100"
+                height="20"
+                fill="#121054"
+            ></rect>
+            <rect
+                y="30"
+                width="100"
+                height="20"
+                fill="#121054"
+            ></rect>
+            <rect
+                y="60"
+                width="100"
+                height="20"
+                fill="#121054"
+            ></rect>
+        </svg>
+        <ul class="hidden md:flex uppercase gap-4">
+            <li>Learn</li>
+            <li>About</li>
+            <li>Contacts</li>
+            <li
+                @click="$store.dispatch('authenticate')"
+                class="text-main-orange"
+            >
+                Join now
+            </li>
+        </ul>
+    </nav>
 </template>
 <script>
-  export default {
-    name: 'MainNavigation'
-  }
+export default {
+    name: 'MainNavigation',
+}
 </script>
