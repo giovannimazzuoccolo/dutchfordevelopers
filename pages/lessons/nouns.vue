3d183746f869b96319ed6e775e444b7d11dc586b
Giovanni Mazzuoccolo <gio.mazzuoccolo@gmail.com>
2022-10-29 22:02:42 +0200

    trying to read markdown content

---
+++ pages/lessons/nouns.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-html="markdownToHtml"></div>
+</template>
+<script lang="ts">
+import marked from 'marked'
+import Vue from 'vue'
+const nouns = require('./../../content/lessons/nouns.md')
+
+Vue.extend({
+    data() {
+        return {
+            text: nouns,
+        }
+    },
+    computed: {
+        markdownToHtml() {
+            return marked(nouns)
+        },
+    },
+})
+</script>
