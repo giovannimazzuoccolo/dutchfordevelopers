a83cbf3dad22f4ac315e1cb96be6bf5b30ac7ed6
Giovanni Mazzuoccolo <gio.mazzuoccolo@gmail.com>
2022-11-27 18:26:05 +0100

    disable disabled buttons

--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -25,7 +25,7 @@ export default defineComponent({
     },
     methods: {
         callback: function (e: MouseEvent) {
-            this.$emit('click', e)
+            !this.disabled && this.$emit('click', e)
         },
     },
 })
