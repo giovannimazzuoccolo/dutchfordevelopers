c27ace0b939062f8b829c73732f4e6910f60fe43
Giovanni Mazzuoccolo <gio.mazzuoccolo@gmail.com>
2023-03-21 22:52:31 +0100

    Finished adjectives, adding games

---
+++ content/bijvoemojilijkList.ts
@@ -0,0 +1,126 @@
+export const adijlist = [
+    {
+      "dutch": "groot",
+      "english": "big",
+      "emoji": "🐘"
+    },
+    {
+      "dutch": "klein",
+      "english": "small",
+      "emoji": "🐭"
+    },
+    {
+      "dutch": "mooi",
+      "english": "beautiful, pretty",
+      "emoji": "💄"
+    },
+    {
+      "dutch": "lelijk",
+      "english": "ugly",
+      "emoji": "🤢"
+    },
+    {
+      "dutch": "snel",
+      "english": "fast, quick",
+      "emoji": "🏎️"
+    },
+    {
+      "dutch": "langzaam",
+      "english": "slow",
+      "emoji": "🐌"
+    },
+    {
+      "dutch": "goed",
+      "english": "good",
+      "emoji": "👍"
+    },
+    {
+      "dutch": "slecht",
+      "english": "bad",
+      "emoji": "👎"
+    },
+    {
+      "dutch": "duur",
+      "english": "expensive",
+      "emoji": "💰"
+    },
+    {
+      "dutch": "goedkoop",
+      "english": "cheap",
+      "emoji": "💸"
+    },
+    {
+      "dutch": "druk",
+      "english": "busy",
+      "emoji": "🕰️"
+    },
+    {
+      "dutch": "rustig",
+      "english": "quiet",
+      "emoji": "🤫"
+    },
+    {
+      "dutch": "zwaar",
+      "english": "heavy",
+      "emoji": "🏋️"
+    },
+    {
+      "dutch": "licht",
+      "english": "light",
+      "emoji": "💡"
+    },
+    {
+      "dutch": "oud",
+      "english": "old",
+      "emoji": "👴"
+    },
+    {
+      "dutch": "nieuw",
+      "english": "new",
+      "emoji": "🆕"
+    },
+    {
+      "dutch": "hard",
+      "english": "hard",
+      "emoji": "🥊"
+    },
+    {
+      "dutch": "zacht",
+      "english": "soft",
+      "emoji": "🧸"
+    },
+    {
+      "dutch": "interessant",
+      "english": "interesting",
+      "emoji": "🤔"
+    },
+    {
+      "dutch": "saai",
+      "english": "boring",
+      "emoji": "😴"
+    },
+    {
+      "dutch": "makkelijk",
+      "english": "easy",
+      "emoji": "👌"
+    },
+    {
+      "dutch": "moeilijk",
+      "english": "difficult",
+      "emoji": "🤯"
+    },
+    {
+      "dutch": "lekker",
+      "english": "tasty, delicious",
+      "emoji": "🍔"
+    },
+    {
+      "dutch": "vies",
+      "english": "dirty, gross",
+      "emoji": "🤮"
+    },
+    {
+      "dutch": "warm",
+      "english": "warm, hot",
+      "emoji": "🔥"
+    }];
